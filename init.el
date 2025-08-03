9ad7d44cf3e9dda856d9c9e74d1a74d43e5ee183
aahsnr <ahsanur041@proton.me>
2025-08-04 02:19:15 +0600

    updated configuration

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
-;;; init.el --- Init -*- lexical-binding: t; -*-
-
+;;; init.el --- loads main config -*- no-byte-compile: t; lexical-binding: t; -*-
+(let ((custom-config (expand-file-name "config.el" user-emacs-directory)))
+  (when (file-exists-p custom-config)
+    (load-file custom-config)))
 ;;; init.el ends here
+
--- post-init.el
+++
@@ -1,5 +0,0 @@
-;;; post-init.el --- Load custom tangled configuration -*- no-byte-compile: t; lexical-binding: t; -*-
-
-(let ((custom-config (expand-file-name "config.el" minimal-emacs-user-directory)))
-  (when (file-exists-p custom-config)
-    (load-file custom-config)))
