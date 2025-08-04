b95e4d2b30cfee04a46feaca1dc9e988f48dc1e7
aahsnr <ahsanur041@proton.me>
2025-08-05 05:28:50 +0600

    updated

--- init.el
+++ init.el
@@ -1,6 +1,5 @@
 ;;; init.el --- loads main config -*- no-byte-compile: t; lexical-binding: t; -*-
 (setq custom-file (locate-user-emacs-file "custom.el")) 
-(load custom-file 'noerror)
 
 (let ((custom-config (expand-file-name "config.el" user-emacs-directory)))
   (when (file-exists-p custom-config)
