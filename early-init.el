8d7410872b1ac54fddecf8d6fa4207075765db9e
aahsnr <ahsanur041@proton.me>
2025-08-05 06:30:09 +0600

    updated

--- early-init.el
+++ early-init.el
@@ -30,16 +30,15 @@
       auto-save-list-file-prefix (expand-file-name "auto-save-list/" no-littering-var-directory)
       auto-save-file-name-transforms `((".*" ,(expand-file-name "auto-saves/" no-littering-var-directory) t))
       server-dir (expand-file-name "server/" no-littering-var-directory)
-      custom-file (expand-file-name "custom.el" no-littering-etc-directory) ; [16]
+      custom-file (expand-file-name "custom.el" no-littering-var-directory) ; [16]
       savehist-file (expand-file-name "savehist" no-littering-var-directory) ; [5, 28]
       recentf-save-file (expand-file-name "recentf" no-littering-var-directory) ; [3, 22]
-      abbrev-file-name (expand-file-name "abbrevs.el" no-littering-etc-directory)
+      abbrev-file-name (expand-file-name "abbrevs.el" no-littering-var-directory)
       bookmark-default-file (expand-file-name "bookmarks" no-littering-var-directory)
       tramp-persistency-file-name (expand-file-name "tramp" no-littering-var-directory))
 
 ;; Configure Elpaca directories. [29, 31]
-(setq elpaca-directory (expand-file-name "elpaca/" user-emacs-directory)
-      package-user-dir (expand-file-name "elpa" no-littering-var-directory)) ; [7, 13]
+(setq package-user-dir (expand-file-name "elpa" no-littering-var-directory)) ; [7, 13]
 
 ;; Persist the scratch buffer across sessions.
 (setq persistent-scratch-save-file (expand-file-name "scratch" no-littering-var-directory))
@@ -93,7 +92,7 @@
 (add-hook 'emacs-startup-hook
           (defun my/restore-startup-settings ()
             "Restore settings that were deferred during startup."
-            (setq gc-cons-threshold (* 100 1024 1024)) ; 100MB
+            (setq gc-cons-threshold (* 2 1024 1024)) ; 100MB
             (setq file-name-handler-alist my/file-name-handler-alist-original)
             (setq read-process-output-max (* 2 1024 1024)) ; 2MB
             (setq-default mode-line-format (default-value 'mode-line-format)))
